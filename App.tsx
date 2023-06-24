fefc0a738196c2951e53e4654adc1bf67973811b
dipakcampus365 <dipak@campus365.io>
2023-06-24 22:29:31 +0530

    initialized app and added react native vector icons

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+      <Text>bvjhchfyfhjvbhjvhjvhjvhmvhjvjfhj ftfyuruyfyugiu</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
